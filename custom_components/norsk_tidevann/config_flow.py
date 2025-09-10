9ead88f7db67ac7a91d5de8ad4797feb142be767
Håvard <146569636+Howard0000@users.noreply.github.com>
2025-09-10 18:36:11 +0200

    Update config_flow.py

--- custom_components/norsk_tidevann/config_flow.py
+++ custom_components/norsk_tidevann/config_flow.py
@@ -3,6 +3,7 @@ from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
+
 class NorskTidevannConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for Norsk Tidevann integration."""
 
@@ -11,14 +12,25 @@ class NorskTidevannConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            return self.async_create_entry(
-                title=user_input["location_name"],
-                data={
-                    "latitude": user_input["latitude"],
-                    "longitude": user_input["longitude"],
-                    "location_name": user_input["location_name"],
-                },
-            )
+            try:
+                lat = float(user_input["latitude"])
+                lon = float(user_input["longitude"])
+            except (KeyError, ValueError, TypeError):
+                errors["base"] = "invalid_coords"
+            else:
+                # Lag en unik ID basert på avrundede koordinater
+                uid = f"{round(lat, 4)}_{round(lon, 4)}"
+                await self.async_set_unique_id(uid)
+                self._abort_if_unique_id_configured()
+
+                return self.async_create_entry(
+                    title=user_input["location_name"],
+                    data={
+                        "latitude": lat,
+                        "longitude": lon,
+                        "location_name": user_input["location_name"],
+                    },
+                )
 
         # Hent standardposisjon fra Home Assistant
         hass: HomeAssistant = self.hass
@@ -33,12 +45,3 @@ class NorskTidevannConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         })
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
-
-
-
-
-
-
-
-
-
